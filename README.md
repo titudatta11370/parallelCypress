07b85d78666e9e73282c30644a28bf95506497d2
titudattaexiger <tdatta@exiger.com>
2019-06-07 12:59:35 -0400

    adding cypress tests

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# parallelCypress
+## installation
+
+```
+npm i
+```
+
+## run tests without gui
+
+```
+npm t
+```
+
+## run tests with gui
+
+```
+npm run cypress open
+```
---
+++ cypress/integration/ThreePM_Home.js
@@ -0,0 +1,75 @@
+describe('Login test case', function () {
+
+    const username = 'support3@exiger.com'
+    const password = 'Admin1234$'
+
+
+    context('Unsuccessful login', function () {
+        beforeEach(function () {
+            cy.visit(Cypress.config().baseUrl)
+        })
+
+        it('displays errors on login for incorrect username/password', function () {
+            // incorrect username on purpose
+
+            cy.server()
+            cy.route({
+                method : 'POST',
+                url : '/Auth/Login',
+                response : '',
+                status : 302
+            })
+
+            cy.get('#UserName').type('Test')
+            cy.get('#auth-continue-button').click()
+            cy.get('#Password').type('password123{enter}')
+            cy.get('#auth-submit-button').click()
+            // we should have visible errors now
+            cy.contains('The username or password you entered is incorrect')
+
+            // and still be on the same URL
+            cy.url().should('include', '/Auth/Login')
+        });
+    })
+
+    context('Successful login', function () {
+        beforeEach(function () {
+            cy.visit(Cypress.config().baseUrl)
+            cy.wait(1000)
+
+            cy.get('#UserName').type(username)
+            cy.get('#auth-continue-button').click()
+            cy.get('#Password').type(password)
+            cy.get('#auth-submit-button').click()
+
+
+            cy.server()
+            cy.route({
+                method : 'POST',
+                url : '/Auth/Login',
+                response : '{"loging":"success"}',
+                status : 302
+            })
+
+
+        })
+
+        it('redirects to /dashboard on success', function () {
+
+            cy.contains('Dashboard')
+        })
+
+        it('Clicking and closing right nav panel', function () {
+
+            cy.get("#thirdPartyTilesContainer").contains("Questionnaires Outstanding").click()
+            cy.document().contains("Order Status")
+            cy.get('#thirdPartyTilesDrawer').contains('Close').click({force: true})
+            cy.document().contains("Order Status").should('not.be.visible')
+        })
+
+        it('should have six right navigation panel', function () {
+            cy.get('#thirdPartyTilesContainer').find('li').should('have.length', 6)
+
+        })
+    })
+})
