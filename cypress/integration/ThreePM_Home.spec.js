31321681a53690fc18ccaade37389800eae3db32
titudattaexiger <tdatta@exiger.com>
2019-06-11 18:11:49 -0400

    login updated

--- cypress/integration/ThreePM_Home.spec.js
+++ cypress/integration/ThreePM_Home.spec.js
@@ -4,85 +4,92 @@ describe('Login test case', function () {
 
 
 
-    // context('Unsuccessful login', function () {
-    //     beforeEach(function () {
-    //         cy.visit(baseURL);
-    //         cy.wait(1000)
-    //     });
-    //
-    //     it('displays errors on login for incorrect username/password', function () {
-    //         // incorrect username on purpose
-    //
-    //         cy.get('#UserName').type('Test');
-    //         cy.get('#auth-continue-button').click();
-    //         cy.get('#Password').type('password123{enter}');
-    //         cy.get('#auth-submit-button').click();
-    //         // we should have visible errors now
-    //         cy.contains('The username or password you entered is incorrect');
-    //
-    //         // and still be on the same URL
-    //         cy.url().should('include', '/Auth/Login')
-    //     });
-    // });
-    //
-    // context('Successful login', function () {
-    //
-    //
-    //     beforeEach(function () {
-    //         cy.visit(baseURL);
-    //         cy.wait(1000);
-    //
-    //         cy.get('#UserName').type(Cypress.env('user'));
-    //         cy.get('#auth-continue-button').click();
-    //         cy.get('#Password').type(Cypress.env('pass'));
-    //         cy.get('#auth-submit-button').click()
-    //
-    //     });
-    //
-    //
-    //     it('redirects to /dashboard on success', function () {
-    //
-    //         cy.document().contains('Dashboard')
-    //     });
-    //
-    //     it('Clicking and closing right nav panel', function () {
-    //
-    //         cy.get("#thirdPartyTilesContainer").contains("Questionnaires Outstanding").click();
-    //         cy.document().contains("Order Status");
-    //         cy.get('#thirdPartyTilesDrawer').contains('Close').click({force: true});
-    //         cy.document().contains("Order Status").should('not.be.visible')
-    //     });
-    //
-    //     it('should have six right navigation panel', function () {
-    //
-    //         cy.get('#thirdPartyTilesContainer').
-    //         find('li').
-    //         should('have.length', 6)
-    //
-    //     });
-    //     it('should be able to sign off', function () {
-    //         cy.contains('Log Off').click({force: true});
-    //         cy.contains('Username').should('be.visible')
-    //
-    //     });
-    //     it('should have the dashboard section metrics', function () {
-    //         cy.get(".dashboard-section-metrics").should('be.visible')
-    //
-    //     });
-    // });
+    context('Unsuccessful login', function () {
+        beforeEach(function () {
+            cy.visit(baseURL);
+            cy.wait(1000)
+        });
+
+        it('displays errors on login for incorrect username/password', function () {
+            // incorrect username on purpose
+
+            cy.get('#UserName').type('Test');
+            cy.get('#auth-continue-button').click();
+            cy.get('#Password').type('password123{enter}');
+            cy.get('#auth-submit-button').click();
+            // we should have visible errors now
+            cy.contains('The username or password you entered is incorrect');
+
+            // and still be on the same URL
+            cy.url().should('include', '/Auth/Login')
+        });
+    });
+
+    context('Successful login', function () {
+
+
+        before(function () {
+            cy.visit(baseURL);
+            cy.wait(1000);
+
+            cy.get('#UserName').type(Cypress.env('user'));
+            cy.get('#auth-continue-button').click();
+            cy.get('#Password').type(Cypress.env('pass'));
+            cy.get('#auth-submit-button').click()
+
+        });
+
+        beforeEach(function () {
+            // before each test, we can automatically preserve the
+            // 'session_id' and 'remember_token' cookies. this means they
+            // will not be cleared before the NEXT test starts.
+            //
+            // the name of your cookies will likely be different
+            // this is just a simple example
+            Cypress.Cookies.preserveOnce('session_id', 'remember_token')
+        })
+
+
+        it('redirects to /dashboard on success', function () {
+
+            cy.document().contains('Dashboard')
+        });
+
+        it('Clicking and closing right nav panel', function () {
+
+            cy.get("#thirdPartyTilesContainer").contains("Questionnaires Outstanding").click();
+            cy.document().contains("Order Status");
+            cy.get('#thirdPartyTilesDrawer').contains('Close').click({force: true});
+            cy.document().contains("Order Status").should('not.be.visible')
+        });
+
+        it('should have six right navigation panel', function () {
+
+            cy.get('#thirdPartyTilesContainer').
+            find('li').
+            should('have.length', 6)
+
+        });
+        it('should be able to sign off', function () {
+            cy.contains('Log Off').click({force: true});
+            cy.contains('Username').should('be.visible')
+
+        });
+        it('should have the dashboard section metrics', function () {
+            cy.get(".dashboard-section-metrics").should('be.visible')
+
+        });
+    });
     context('Home page functionalities', function () {
 
         beforeEach(function () {
-            cy.login(Cypress.env('user'), Cypress.env('pass'))
-            // Cypress.loing(Cypress.env('user'), Cypress.env('pass'))
-            //
-            // cy.visit(baseURL);
-            // cy.wait(1000);
-            //
-            // cy.get('#UserName').type(Cypress.env('user'));
-            // cy.get('#auth-continue-button').click();
-            // cy.get('#Password').type(Cypress.env('pass'));
-            // cy.get('#auth-submit-button').click()
+            cy.visit(baseURL);
+            cy.wait(1000);
+
+            cy.get('#UserName').type(Cypress.env('user'));
+            cy.get('#auth-continue-button').click();
+            cy.get('#Password').type(Cypress.env('pass'));
+            cy.get('#auth-submit-button').click()
 
         });
 
