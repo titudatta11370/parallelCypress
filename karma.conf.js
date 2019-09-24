ca49f9c9b873919bfb75faa870dbf90b607a9433
titudattaexiger <tdatta@exiger.com>
2019-09-24 17:01:33 -0400

    circle.yml added

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,12 @@
 customLaunchers: {
-  ChromeHeadless: {
-    base: 'Chrome',
-    flags: [
-      '--headless',
-      '--disable-gpu',
-      '--no-sandbox',
-      '--remote-debugging-port=9222',
-    ]
-  }
-},
-browsers: ['ChromeHeadless'],
+    ChromeHeadless: {
+      base: 'Chrome',
+      flags: [
+        '--headless',
+        '--disable-gpu',
+        '--no-sandbox',
+        '--remote-debugging-port=9222',
+      ]
+    }
+  },
+  browsers: ['ChromeHeadless'],
